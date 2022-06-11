281aead12986845e3a42f4ff714b6e82c32ae8e9
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-06-11 13:08:31 +0600

    new php code

---
+++ strings.php
@@ -0,0 +1,40 @@
+<?php
+
+echo "Hi, my name is Sourav " . "Majumder";
+echo "<br>";
+echo "I am 'whaa'";
+echo "<br>";
+
+echo "I am sourav \"sourav\" majumder";
+echo "<br>";
+$name1 = 'mahim';
+$name2 = 'MAHIM';
+
+echo "My name is $name1";
+echo "<br>";
+
+echo "My length is " . strlen($name1);
+echo "<br>";
+echo "My toupper is " . strtoupper($name1);
+echo "<br>";
+echo "My tolower is " . strtolower($name2);
+echo "<br>";
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
